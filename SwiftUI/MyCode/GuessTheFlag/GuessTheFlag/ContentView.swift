fb6b4058d9f87cca6470ad312b5e23d2f991362c
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-08-19 18:57:16 +0200

    Alert

--- SwiftUI/MyCode/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ SwiftUI/MyCode/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,40 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Button("Button 1") {}
-                .buttonStyle(.bordered)
-            Button("Button 2", role: .destructive) {}
-                .buttonStyle(.bordered)
-            Button("Button 3") {}
-                .buttonStyle(.borderedProminent)
-            Button("Button 4", role: .destructive) {}
-                .buttonStyle(.borderedProminent)
-            Button("Button 5") {}
-                .buttonStyle(.borderedProminent)
-                .tint(.mint)
-            Button {
-                print("Custom button was tapped")
-            } label: {
-                Text("Tap me!")
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(.red)
-            }
-            Button {
-                print("Edit button was tapped")
-            } label: {
-                //Image(systemName: "pencil")
-                
-                // Label is neater than HStack of image and label
-                Label("Edit", systemImage: "pencil")
-            }
-        }
-    }
+    @State private var showingAlert = false
     
-    func executeDelete() {
-        print("Now deleting")
+    var body: some View {
+        Button("Show Alert") {
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) {}
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Please read this")
+        }
     }
 }
 
