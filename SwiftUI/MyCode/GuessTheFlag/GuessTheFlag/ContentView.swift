f7327550fa9a3b271d999f50f14cf168b77294eb
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-08-19 18:53:09 +0200

    buttons

--- SwiftUI/MyCode/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ SwiftUI/MyCode/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("Delete selection", role: .destructive, action: executeDelete)
+        VStack {
+            Button("Button 1") {}
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) {}
+                .buttonStyle(.bordered)
+            Button("Button 3") {}
+                .buttonStyle(.borderedProminent)
+            Button("Button 4", role: .destructive) {}
+                .buttonStyle(.borderedProminent)
+            Button("Button 5") {}
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            Button {
+                print("Custom button was tapped")
+            } label: {
+                Text("Tap me!")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.red)
+            }
+            Button {
+                print("Edit button was tapped")
+            } label: {
+                //Image(systemName: "pencil")
+                
+                // Label is neater than HStack of image and label
+                Label("Edit", systemImage: "pencil")
+            }
+        }
     }
     
     func executeDelete() {
