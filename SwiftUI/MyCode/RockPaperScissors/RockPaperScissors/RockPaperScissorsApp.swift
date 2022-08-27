5f248d932c5ef5c3552fe0a822cafc6e7b164dcb
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-08-27 18:19:20 +0200

    Create challenge project

---
+++ SwiftUI/MyCode/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Oscar Fridh on 2022-08-27.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
