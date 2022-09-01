510f95852ec8188d91667f1f202e33af9e639958
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-09-01 20:48:38 +0200

    working with dates

--- SwiftUI/MyCode/BetterRest/BetterRest/ContentView.swift
+++ SwiftUI/MyCode/BetterRest/BetterRest/ContentView.swift
@@ -10,9 +10,27 @@ import SwiftUI
 struct ContentView: View {
     @State private var wakeUp = Date.now
     
+    func test() {
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+        let date = Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    func test2() {
+        let someDate = Date.now
+        let components = Calendar.current.dateComponents([.hour, .minute], from: someDate)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+    }
+    
     var body: some View {
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-            .labelsHidden()
+        VStack {
+            Text(Date.now, format: .dateTime.year().hour())
+            Text(Date.now.formatted(date: .long, time: .shortened))
+            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+                .labelsHidden()
+        }
     }
     
     
