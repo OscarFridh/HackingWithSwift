1530ecbb08d99a9f58d20d0630a1cbee6d6ca685
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-09-04 18:43:38 +0200

    create project

---
+++ SwiftUI/MyCode/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Oscar Fridh on 2022-09-04.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
