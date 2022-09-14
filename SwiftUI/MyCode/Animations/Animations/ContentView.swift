0525f8c69c9bdb9f2184fab6c8171370ce9b2e49
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-09-14 13:01:11 +0200

    explicit animations

--- SwiftUI/MyCode/Animations/Animations/ContentView.swift
+++ SwiftUI/MyCode/Animations/Animations/ContentView.swift
@@ -11,27 +11,19 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        print(animationAmount)
-        
-        return VStack {
-            Stepper("Scale amount", value:
-                        $animationAmount.animation(
-                            .easeInOut(duration: 1)
-                            .repeatCount(3, autoreverses: true)
-                        )
-                    , in: 1...10)
-            
-            Spacer()
-            
-            Button("Tap me") {
-                animationAmount += 1
+        Button("Tap me") {
+            withAnimation(
+                .interpolatingSpring(stiffness: 5, damping: 1)
+//                .easeInOut(duration: 1)
+            ) {
+                animationAmount += 180
             }
-            .padding(40)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
         }
+        .padding(40)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 1, z: 0))
     }
 }
 
