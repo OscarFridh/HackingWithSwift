f5f56b2769686cf259011da87e0c030dc8a413ef
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-10-03 11:25:20 +0200

    created new project

---
+++ SwiftUI/MyCode/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Oscar Fridh on 2022-10-03.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
