cd72b3ffc4b4f3bd70daa83621c0eba0ca44a644
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-10-06 21:03:50 +0200

    Navigation view

--- SwiftUI/MyCode/Moonshot/Moonshot/ContentView.swift
+++ SwiftUI/MyCode/Moonshot/Moonshot/ContentView.swift
@@ -9,28 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 10) {
-                ForEach(0..<100) {
-                    CustomText("Item \($0)")
-                        .font(.title)
-                }
+        NavigationView {
+            NavigationLink {
+                Text("Detail View")
+            } label: {
+                Text("Hello, world!")
+                    .navigationTitle("Swift UI")
             }
-            .frame(maxWidth: .infinity)
         }
-    }
-}
-
-struct CustomText: View {
-    let text: String
-    
-    var body: some View {
-        Text(text)
-    }
-    
-    init(_ text: String) {
-        print("Creating a new CustomText")
-        self.text = text
     }
 }
 
