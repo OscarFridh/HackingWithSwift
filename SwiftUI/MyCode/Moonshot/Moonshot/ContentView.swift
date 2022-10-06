f7794674df55943f4d8a800fb09e646e523dcf20
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-10-06 21:05:03 +0200

    NavigationView containing a List

--- SwiftUI/MyCode/Moonshot/Moonshot/ContentView.swift
+++ SwiftUI/MyCode/Moonshot/Moonshot/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            NavigationLink {
-                Text("Detail View")
-            } label: {
-                Text("Hello, world!")
-                    .navigationTitle("Swift UI")
+            List(0..<100) { row in
+                NavigationLink {
+                    Text("Detail \(row)")
+                } label: {
+                    Text("Row \(row)")
+                }
             }
+            .navigationTitle("Swift UI")
         }
     }
 }
