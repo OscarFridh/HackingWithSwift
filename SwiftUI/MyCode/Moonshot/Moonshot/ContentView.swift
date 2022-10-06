c734c9e68e0ef795372c8051cf6dd9d6f70dc0dd
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-10-06 20:49:30 +0200

    simple scroll view

--- SwiftUI/MyCode/Moonshot/Moonshot/ContentView.swift
+++ SwiftUI/MyCode/Moonshot/Moonshot/ContentView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image("Example")
-                .resizable()
-                .scaledToFit()
-                .frame(width: geo.size.width * 0.8, height: 300)
-                .background(Color.green)
-                .frame(width: geo.size.width, height: geo.size.height)
-                .background(Color.red)
+        ScrollView {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    Text("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
     }
 }
