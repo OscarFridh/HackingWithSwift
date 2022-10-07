c9dffa34dccbbe31880b37b39d87bfb6a654a0bc
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-10-07 11:04:01 +0200

    Generic decoding used for both astronauts and missions

---
+++ SwiftUI/MyCode/Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Oscar Fridh on 2022-10-07.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
